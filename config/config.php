418c63b445f06df4564e2fd3018b3d15647b8d1a
Nyonga Lizphill Lumah Vense'em <96431152+Lizphill@users.noreply.github.com>
2024-09-03 14:02:48 +0100

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+//host
+
+$host = "localhost";
+
+//dbname
+
+$dbname = "Bookstore";
+
+//username
+
+$user = "root";
+
+//password
+
+$pass = "";
+
+$conn = new PDO ("mysql:host=$host;dbname=$dbname",$user,$pass);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// if($conn) {
+//     echo "Worked";
+// } else {
+//     echo "error in connection";
+// }
